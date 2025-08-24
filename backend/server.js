29d499bc3d87a0ffa278e3d50983d7a0f37d01f5
Carlos <jcpacc6@outlook.com>
2025-08-24 13:26:06 -0400

    fixing

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const originUrl = process.env.ORIGIN_URL || "http://localhost:5173"
 // MIDDLEWARES
 app.use(express.json())
 app.use(cors({
-  origin: originUrl, // or your frontend URL
+  origin: "https://workout-xi-roan.vercel.app", // or your frontend URL
   credentials: true// allow cookies to be sent
 }));
 app.use(cookieParser())
