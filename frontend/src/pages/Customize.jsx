d8917f5756b7125d7ec1b41925c334e488621470
Carlos <jcpacc6@outlook.com>
2025-09-01 11:24:24 -0400

    sep

---
+++ frontend/src/pages/Customize.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Exercises from "../components/Exercises.jsx";
+import Workout from "../components/Workout.jsx";
+
+const Customize = () => {
+  return (
+    <div>
+      <Exercises />
+      <Workout />
+    </div>
+  );
+};
+
+export default Customize;
---
+++ frontend/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // relative paths, works when served from a subfolder,
+  build: {
+    outDir: '../backend/dist',   // relative to frontend folder
+    emptyOutDir: true,   // cleans it before rebuilding
+  },
+})
